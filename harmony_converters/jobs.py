07d97a10b94f08c50406be95a2386e738d045c1c
Christophe Benz <cbenz@easter-eggs.com>
2013-02-26 17:41:08 +0100

    Fix syntax.

--- harmony_converters/jobs.py
+++ harmony_converters/jobs.py
@@ -10,4 +10,6 @@ scripts_base_dirname = os.path.join(os.dirname(__file__), '..', 'scripts')
 
 def start(job_name, project_id, callback_url, *args):
     job_script_filepath = os.path.join(scripts_base_dirname, '{0}.py'.format(job_name))
-    return subprocess.Popen(['python', job_script_filepath, '--callback-url', callback_url, project_id, *args])
+    arguments = ['python', job_script_filepath, '--callback-url', callback_url, project_id]
+    arguments.extend(args)
+    return subprocess.Popen(arguments)
