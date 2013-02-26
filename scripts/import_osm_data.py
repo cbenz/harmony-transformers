b0d813ca895151fb114165bd975558cb2a6401c8
Christophe Benz <cbenz@easter-eggs.com>
2013-02-26 21:10:18 +0100

    Change tree, everything is in data/projects.

--- scripts/import_osm_data.py
+++ scripts/import_osm_data.py
@@ -16,17 +16,16 @@ def main(args=None):
     parser = argparse.ArgumentParser(description=u'Import OSM data file into PostGIS database.')
     parser.add_argument('project_id')
     parser.add_argument('osm_data_filepath')
-    parser.add_argument('cache_base_dirname')
+    parser.add_argument('cache_dirname')
     parser.add_argument('mapping_filepath')
     parser.add_argument('user'),
     parser.add_argument('password'),
     parser.add_argument('--callback-url')
     arguments = parser.parse_args(args)
-    cache_dir = os.path.join(arguments.cache_base_dirname, arguments.project_id)
-    if not os.path.exists(cache_dir):
-        os.mkdir(cache_dir)
+    if not os.path.exists(arguments.cache_dirname):
+        os.mkdir(arguments.cache_dirname)
     imposm.app.main([
-        '--cache-dir={0}'.format(cache_dir),
+        '--cache-dir={0}'.format(arguments.cache_dirname),
         '--connection=postgis://{arguments.user}:{arguments.password}@localhost:5432/{arguments.project_id}'.format(
             arguments=arguments,
             ),
