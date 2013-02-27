8378c070297976196c9da52ba0650cafb2bf2bf0
Romain Soufflet <rsoufflet@easter-eggs.com>
2013-02-27 11:07:07 +0100

    Adapt shapefile_to_osm_data scripts to update information about jobs.

--- scripts/shapefile_to_osm_data.py
+++ scripts/shapefile_to_osm_data.py
@@ -21,33 +21,37 @@ log = logging.getLogger('harmonyconverters.wrapper')
 def main():
     parser = argparse.ArgumentParser(description = __doc__)
     parser.add_argument('project_id', help = "Project ID sequence")
+    parser.add_argument('--callback-url', help = "Callback URL")
+    parser.add_argument('base_dirname', help = "Path to cache directory")
     parser.add_argument('ogr2osm_script', help = "Path to shapefile file")
     parser.add_argument('shapefile', help = "Path to shapefile file")
-    parser.add_argument('cache_base_dirname', help = "Path to cache directory")
-    parser.add_argument('callback_url', help = "Callback URL")
     parser.add_argument('-v', '--verbose', action = 'store_true', help = 'increase output verbosity')
 
     args = parser.parse_args()
     logging.basicConfig(level = logging.DEBUG if args.verbose else logging.WARNING, stream = sys.stdout)
 
-    output_file = open(os.path.join(args.cache_base_dirname, 'stdout', args.project_id), 'w')
-    error_file = open(os.path.join(args.cache_base_dirname, 'stderr', args.project_id), 'w')
-    bash_process = subprocess.Popen(
-        ['bash'],
+    output_file = open(os.path.join(args.base_dirname, 'cache', 'stdout', args.project_id), 'w')
+    error_file = open(os.path.join(args.base_dirname, 'cache', 'stderr', args.project_id), 'w')
+
+    job_process = subprocess.Popen(
+        ['python', args.ogr2osm_script, args.shapefile],
         stdin = subprocess.PIPE,
         stdout = output_file,
         stderr = error_file,
         )
-    stdoutdata, stderrdata = bash_process.communicate(
-        input = 'python {0} {1}'.format(args.ogr2osm_script, args.shapefile)
-        )
+
     output_file.close()
     error_file.close()
 
-    if bash_process.returncode != 0:
-        log.error(u'Failed to launch subprocess', stderrdata)
-    else:
-        log.info('Process successfully launched')
+    lock_file_path = os.path.join(args.base_dirname, 'cache', 'locks', args.project_id)
+    with open(lock_file_path, 'w') as lock_file:
+        lock_file.write(str(job_process.pid))
+
+    job_process.wait()
+
+    status_file_path = os.path.join(args.base_dirname, 'cache', 'statuses', args.project_id)
+    with open(status_file_path, 'w') as status_file:
+        status_file.write(str(job_process.returncode))
 
     try:
         response = urllib2.urlopen(args.callback_url)
@@ -59,9 +63,9 @@ def main():
         raise
     log.info('Callback URL returned {0} code'.format(response.code))
 
-    lock_file_path = os.path.join(args.cache_base_dirname, 'locks', args.project_id)
     if os.path.isfile(lock_file_path):
-        log.info('Should remove {0}'.format(lock_file_path))
+        os.unlink(lock_file_path)
+        log.info('Lock file {0} removed'.format(lock_file_path))
     return 0
 
 
