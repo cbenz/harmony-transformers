d30be43e0d1a4c3f1384f1dae74586666abbcf38
Christophe Benz <cbenz@easter-eggs.com>
2013-02-28 18:46:24 +0100

    Commit data dir.

---
+++ data/carto_project_template/style.mss
@@ -0,0 +1,6 @@
+#nodes {
+  marker-width:6;
+  marker-fill:#f45;
+  marker-line-color:#813;
+  marker-allow-overlap:true;
+}
---
+++ data/imposm_mapping_template.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
+from imposm.mapping import LineStrings, LocalizedName, Options, Points, set_default_name_type, String
+
+
+db_conf = Options(
+    sslmode='allow',
+    prefix='osm_new_',
+    proj='epsg:900913',
+    )
+
+
+nodes = Points(
+    fields=(
+        {fields}
+        ),
+    mapping={{
+        '{unique_id_tag_key}': (
+            '__any__',
+            ),
+        }},
+    name='nodes',
+    with_type_field=False,
+    )
+
+
+ways = LineStrings(
+    fields=(
+        {fields}
+        ),
+    mapping={{
+        '{unique_id_tag_key}': (
+            '__any__',
+            ),
+        }},
+    name='ways',
+    with_type_field=False,
+    )
